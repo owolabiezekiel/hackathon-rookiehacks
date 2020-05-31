b4428020b96eeca75c2ec4da3928ebaf64095eb6
Megan Thong <meganthong@outlook.com>
2020-05-31 09:55:12 +0800

    Add files via upload

---
+++ src/components/UserHome.js
@@ -0,0 +1,12 @@
+import React from 'react';
+ 
+const UserHome = () => {
+    return (
+       <div>
+          <h1>Welcome Home</h1>
+          <p>Here is the latest posts from the forums and users you follow.</p>
+       </div>
+    );
+}
+ 
+export default UserHome;
